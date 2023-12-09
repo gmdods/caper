871890f6914ea09ffa02f31219c0e9a0382d4791
gmd01bit <gmd01bit@gmail.com>
2023-12-10 00:18:03 +0200

    :rotating_light: Fix Documenter git

--- docs/make.jl
+++ docs/make.jl
@@ -3,5 +3,7 @@ using Documenter, Caper
 makedocs(
 	modules=[Caper],
 	sitename="Caper",
-	doctest=true
+	doctest=true,
+	format=Documenter.HTML(;edit_link="master"),
+	repo=Remotes.GitHub("gmdods", "caper")
 )
