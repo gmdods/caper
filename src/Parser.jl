7ae1313532c185f3b37b46b051b801c19bff03b1
gmd01bit <gmd01bit@gmail.com>
2023-12-10 03:51:18 +0200

    :art: Using multiple dispatch in Parser

--- src/Parser.jl
+++ src/Parser.jl
@@ -3,6 +3,46 @@
 const Binary = Symbol.([collect(MathChar); collect(CmpChar) .* '='])
 const Assign = Symbol.(['='; collect(MathChar) .* '='])
 
+
+_ingest(next::AbstractString, stack) = push!(stack, next)
+
+function _ingest(next::T, stack) where {T<:Number}
+        if length(stack) >= 2 && stack[end] in Binary
+                op = pop!(stack)
+                prev = pop!(stack)
+                node = (op, prev, next)
+        else
+                node = next
+        end
+        push!(stack, node)
+
+end
+
+function _ingest(next::Symbol, stack)
+        if next == Symbol(";")
+                @assert length(stack) > 0
+                stmt = pop!(stack)
+                if stack[end] in Assign
+                        equal = pop!(stack)
+                        name = pop!(stack)
+                        @assert name isa AbstractString
+                        node = (next, name, equal, stmt)
+                elseif stack[end] in Keywords
+                        keyword = pop!(stack)
+                        node = (next, Symbol(keyword), stmt)
+                else
+                        node = (next, stmt)
+                end
+                push!(stack, node)
+        elseif next in Binary || next in Assign
+                push!(stack, next)
+        else
+                arg = pop!(stack)
+                push!(stack, (next, arg))
+        end
+end
+
+
 """
 Methods for parsing files
 """
@@ -28,43 +68,10 @@ julia> Caper.ast("return 1 + h'1f';")
 ```
 """
 function ast(text::AbstractString)
-	stack = Any[]
-	for next = lex(text)
-		if next isa AbstractString
-			push!(stack, next)
-		elseif next isa Number
-			if length(stack) >= 2 && stack[end] in Binary
-				op = pop!(stack)
-				prev = pop!(stack)
-				node = (op, prev, next)
-			else
-				node = next
-			end
-			push!(stack, node)
-		elseif next == Symbol(";")
-			@assert length(stack) > 0
-			stmt = pop!(stack)
-			if stack[end] in Assign
-				equal = pop!(stack)
-				name = pop!(stack)
-				@assert name isa AbstractString
-				node = (next, name, equal, stmt)
-			elseif stack[end] in Keywords
-				keyword = pop!(stack)
-				node = (next, Symbol(keyword), stmt)
-			else
-				node = (next, stmt)
-			end
-			push!(stack, node)
-		elseif next isa Symbol
-			if next in Binary || next in Assign
-				push!(stack, next)
-			else
-				arg = pop!(stack)
-				push!(stack, (next, arg))
-			end
-		end
-	end
-	return stack
+        stack = Any[]
+        for next = lex(text)
+                _ingest(next, stack)
+        end
+        return stack
 end
 
